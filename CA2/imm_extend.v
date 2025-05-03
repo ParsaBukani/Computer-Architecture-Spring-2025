f91cb2858468328605ec9453284bbdac4592a844
miladmazaheri <miladmazaheri1383@gmail.com>
2025-05-03 17:46:50 +0330

    update imm_extend

--- CA2/imm_extend.v
+++ CA2/imm_extend.v
@@ -7,13 +7,13 @@ module ImmExtend (
 );
     always @(*) begin
         case (sl)
-            3'b000 : alu 
-            3'b001 : 
-            3'b010 : 
-            3'b011 : 
-            3'b100 : 
+            3'b000 : ImmOut = {20{immData[24]}, immData[24:13]};                                    // I_Type
+            3'b001 : ImmOut = {20{immData[24]}, immData[24:18], immData[4;0]};                      // S_Type
+            3'b010 : ImmOut = {20{immData[24]} ,immData[0], immData[23:18], immData[4:1], 1'b0};    // B_Type
+            3'b011 : ImmOut = {immData[24:5], 12'b0};                                               // U_Type
+            3'b100 : ImmOut = {12{immData[24]} ,immData[12:5], immData[13], immData[23:14], 1'b0};  // J_Type
             default:
-            ImmOut = 32'b0;
+                ImmOut = 32'b0;
         endcase
     end
     
